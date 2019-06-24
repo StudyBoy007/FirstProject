34fc44c6ec4caf39a0e3dd9d6274a27f0804cd3c
StudyBoy007 <1045112166@qq.com>
2019-06-24 15:17:54 +0800

    -

--- src/com/ecjtu/Collection/map/Test.java
+++ src/com/ecjtu/Collection/map/Test.java
@@ -11,9 +11,9 @@ public class Test {
     public static void main(String[] args) {
         Set<Integer> set = new HashSet<>();
         set.add(1);
-        set.add(2);
         set.add(3);
-        set.add(4);
+        set.add(2);
+        set.add(7);
         for (Integer s:set){
             System.out.println(s);
         }
