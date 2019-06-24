05cd9a54590a948640c1922c1d9ef094970caf70
StudyBoy007 <1045112166@qq.com>
2019-06-24 15:01:48 +0800

    上传项目

--- src/com/ecjtu/Collection/map/Test.java
+++ src/com/ecjtu/Collection/map/Test.java
@@ -1,8 +1,21 @@
 package com.ecjtu.Collection.map;
 
+import java.util.HashSet;
+import java.util.Set;
+
 /**
  * Create by czq
  * time on 2019/6/24  14:54
  */
 public class Test {
+    public static void main(String[] args) {
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(2);
+        set.add(3);
+        set.add(4);
+        for (Integer s:set){
+            System.out.println(s);
+        }
+    }
 }
