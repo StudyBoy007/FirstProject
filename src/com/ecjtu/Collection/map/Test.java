0e295c4687241d898e6a3e72e5cd665559f14640
StudyBoy007 <1045112166@qq.com>
2019-06-24 16:48:39 +0800

    上传项目

--- src/com/ecjtu/Collection/map/Test.java
+++ src/com/ecjtu/Collection/map/Test.java
@@ -9,22 +9,22 @@ import java.util.Set;
  */
 public class Test {
     public static void main(String[] args) {
-//        Set<Integer> set = new HashSet<>();
-//        set.add(1);
-//        set.add(3);
-//        set.add(2);
-//        set.add(7);
-//        for (Integer s:set){
-//            System.out.println(s);
-//        }
-
-        Set<String> set = new HashSet<>();
-        set.add("String1");
-        set.add("String4");
-        set.add("String3");
-        set.add("String2");
-        for (String s : set) {
+        Set<Integer> set = new HashSet<>();
+        set.add(1);
+        set.add(3);
+        set.add(2);
+        set.add(7);
+        for (Integer s:set){
             System.out.println(s);
         }
+
+//        Set<String> set = new HashSet<>();
+//        set.add("String1");
+//        set.add("String4");
+//        set.add("String3");
+//        set.add("String2");
+//        for (String s : set) {
+//            System.out.println(s);
+//        }
     }
 }
